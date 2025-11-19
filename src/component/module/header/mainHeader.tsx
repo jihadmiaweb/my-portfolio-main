a782605ae6a036d216e1af218cdb7fe2ea52e073
jihad mia <jihadmiaweb@gmail.com>
2025-11-19 11:34:10 +0600

    ain

--- src/component/module/header/mainHeader.tsx
+++ src/component/module/header/mainHeader.tsx
@@ -100,7 +100,7 @@ export const Mainheader: React.FC = () => {
 
                         <li>
                             <a
-                                href="https://drive.google.com/file/d/1L_rZeQxlryPTvvfBWCTgoaPjpbjTxc2-/view"
+                                href="https://drive.google.com/drive/folders/1SN66Qh0eEDfX3V9PZ15knTO81fUGz2Hf"
                                 onClick={handleNavLinkClick}
                                 className="inline-block px-5 py-2 mt-4 md:mt-0 text-white font-bold text-sm rounded-full bg-gradient-to-r from-pink-600 to-purple-600 hover:from-pink-500 hover:to-purple-500 transition-all duration-300 shadow-xl hover:shadow-pink-500/50 transform hover:scale-105"
                             >
